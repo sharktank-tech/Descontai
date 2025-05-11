c84fba0a40bcf4f32b6c2c557181b2d73e7a97e2
sharktank-tech <danilokog652@gmail.com>
2025-05-11 15:39:35 -0300

    add favi-icon

--- web/views/routes.py
+++ web/views/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, request, redirect
+from flask import render_template, Blueprint, url_for
 
 main_blueprint = Blueprint('main', __name__)
 AFILIADO_TAG = 'ofertasflashh-20'
