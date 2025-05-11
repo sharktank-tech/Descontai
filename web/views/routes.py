b88d027c0a1d9f8db57099da6f9504f1dffa073e
sharktank-tech <danilokog652@gmail.com>
2025-05-11 15:16:04 -0300

    remoção da função search

--- web/views/routes.py
+++ web/views/routes.py
@@ -8,13 +8,3 @@ AFILIADO_TAG = 'ofertasflashh-20'
 def home():
     return render_template('index.html')
 
-
-@main_blueprint.route('/search', methods=['GET', 'POST'])
-def search():
-    termo = request.args.get("q", "")
-    departamento = request.args.get("departamento", "")
-
-    termo_formatado = termo.replace(" ", "+")
-    url_amazon = f"https://www.amazon.com.br/s?k={termo_formatado}&i={departamento}&tag={AFILIADO_TAG}"
-
-    return redirect(url_amazon)
