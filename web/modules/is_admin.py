ca19e2581dcd9a41832f8f227b6420879de5b3b9
Danilo Cavalcante <82549409+sharktank-tech@users.noreply.github.com>
2025-06-24 10:56:13 -0300

    Create is_admin.py

---
+++ web/modules/is_admin.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from flask_login import current_user
+from functools import wraps
+
+def admin_required(f):
+    @wraps(f)  # Mantém o nome original da função
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated or not current_user.is_admin:
+            return render_template('erros/403.html'), 403
+        return f(*
+                 args, **kwargs)
+    return wrapper
